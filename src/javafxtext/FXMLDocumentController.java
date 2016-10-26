97c9c6977bbec8d311c0cf4f8b65b2d1b974c810
Leif Berg <leif.berg@gmail.com>
2016-10-25 20:47:14 -0600

    added function to limit how many children can be expanded at once.
    not sure this is useful feature, but might be worth including.

--- src/javafxtext/FXMLDocumentController.java
+++ src/javafxtext/FXMLDocumentController.java
@@ -8,12 +8,21 @@ package javafxtext;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.ResourceBundle;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 
 /**
@@ -26,7 +35,9 @@ public class FXMLDocumentController {
     private Label filePathLabel;
     
     @FXML
-    private TreeView myTreeView;
+    public TreeView myTreeView;
+    
+    TreeItem<String> root;
 
     @FXML
     private void fileChooserDialog(ActionEvent event) throws Exception {
@@ -39,7 +50,7 @@ public class FXMLDocumentController {
     private void readInFile(String filename) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
         String line;
-        TreeItem<String> root = new TreeItem<>("root");
+        root = new TreeItem<>("root");
         myTreeView.showRootProperty().set(false);
         while ((line = bufferedReader.readLine()) != null)
         {
@@ -48,6 +59,7 @@ public class FXMLDocumentController {
         }
         bufferedReader.close();
         myTreeView.setRoot(root);
+        makeOnlyOneChildExpandableAtATime(root);
     }   
     
     private int getIndexbyValue(TreeItem<String> parent,String val){
@@ -70,4 +82,27 @@ public class FXMLDocumentController {
             addNodeFromString(newParent,subset);
         }  
     }
-}
+    /*@FXML
+    private void mouseClicked(MouseEvent mouseEvent){
+        TreeItem<String> item = (TreeItem<String>) myTreeView.getSelectionModel().getSelectedItem();
+        String selectedItem = item.getValue();
+        //System.out.println(selectedItem);
+        for (TreeItem<String> child : root.getChildren()){
+            if(true){
+                //System.out.println(child.getValue());
+            }
+        }*/
+    private void makeOnlyOneChildExpandableAtATime(TreeItem<String> parent){
+        for (TreeItem<String> child : parent.getChildren()){
+            child.expandedProperty().addListener((Observable observable) -> {
+                if (child.isExpanded()) {
+                    for (TreeItem<String> thisChild : parent.getChildren()){
+                        if(!child.equals(thisChild)){
+                            thisChild.setExpanded(false);
+                        }
+                    }
+                }
+            });
+        }
+    }
+    }
